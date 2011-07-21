166335f1c6d07455e82ea05c7196733441ae3cdb
James Healy <jimmy@deefa.com>
2011-07-22 01:14:24 +1000

    fix the path to our rake tasks

--- lib/db2fog/railtie.rb
+++ lib/db2fog/railtie.rb
@@ -5,7 +5,7 @@ class DB2Fog
   class Railtie < Rails::Railtie
 
     rake_tasks do
-      load File.expand_path('./tasks.rb', __FILE__)
+      load File.expand_path('tasks.rb', File.dirname(__FILE__))
     end
   end
 end
