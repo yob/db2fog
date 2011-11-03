13bb9afaffaaf1f82c045fdc95178018f63de0e5
James Healy <jimmy@deefa.com>
2011-11-03 20:42:09 +1100

    remove descriptions from deprecated rake tasks
    * they'll work but not appear in 'rake -T'

--- lib/db2fog/tasks.rb
+++ lib/db2fog/tasks.rb
@@ -17,19 +17,16 @@ namespace :db2fog do
   end
 
   namespace :backup do
-    desc "Save a full back to S3"
     task :full => :environment do
       $stderr.puts "the db2fog:backup:full rake task is deprecated, use db2fog:backup instead"
       DB2Fog.new.backup
     end
 
-    desc "Restore your DB from S3"
     task :restore => :environment do
       $stderr.puts "the db2fog:backup:restore rake task is deprecated, use db2fog:restore instead"
       DB2Fog.new.restore
     end
 
-    desc "Keep all backups for the last day, one per day for the last week, and one per week before that. Delete the rest."
     task :clean => :environment do
       $stderr.puts "the db2fog:backup:clean rake task is deprecated, use db2fog:clean instead"
       DB2Fog.new.clean
